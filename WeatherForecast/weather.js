327805f2b058b98571fd94d14ccc0200c8fc665b
Maheen Raza <124949405+maheenraza03@users.noreply.github.com>
2025-03-22 19:25:41 +0000

    adding hourly weather function

--- WeatherForecast/weather.js
+++ WeatherForecast/weather.js
@@ -51,3 +51,15 @@ function parseDailyWeather({ daily }) {
     })
 }
 
+function parseHourlyWeather({ hourly, current_weather }) {
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weatherCode[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.windspeed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100
+        }
+    }).filter(({ timestamp }) => timestamp >= current_weather.time * 1000)
+}
