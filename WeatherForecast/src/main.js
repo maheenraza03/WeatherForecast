ecfe28476314593f928999ac95e0424ecf708282
Maheen Raza <124949405+maheenraza03@users.noreply.github.com>
2025-03-22 00:28:42 +0000

    finishing up for today

--- WeatherForecast/src/main.js
+++ WeatherForecast/src/main.js
@@ -1,6 +1,6 @@
 import "style.css"
 import {getWeather} from "./weather"
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timzeZone).then(res => {
-    console.log(res.data)
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timzeZone).then(data => {
+    console.log(data)
 })_
--- WeatherForecast/weather.js
+++ WeatherForecast/weather.js
@@ -1,5 +1,3 @@
-// https://customer-api.open-meteo.com/v1/forecast?daily=weather_code,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,temperature_2m_min,precipitation_sum&hourly=temperature_2m,precipitation,apparent_temperature,weather_code,wind_speed_10m&current=temperature_2m,precipitation,wind_speed_10m,apparent_temperature&timeformat=unixtime
-
 import axios from axios
 
 export function getWeather(lat, lon, timezone) {
@@ -17,16 +15,32 @@ export function getWeather(lat, lon, timezone) {
 }
 
 function parseCurrentWeather({ current_weather, daily}) {
-    const { } = current_weather
+    const { 
+        temperature: currentTemp, 
+        windspeed: windSpeed, 
+        weatherCode: iconCode 
+    } = current_weather
+
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLikeTemp],
+        apparent_temperature_min: [minFeelsLikeTemp],
+        precipitation_sum: [precip],
+    } = daily
     
     return {
-        currentTemp: ,
-        highTemp: ,
-        lowTemp: ,
-        highFeelsLike: ,
-        lowFeelsLike: 
-        windSpeed: ,
-        precip: ,
-        iconCode: ,
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        highFeelsLike: Math.round(maxFeelsLikeTemp),
+        lowFeelsLike: Math.round(minFeelsLikeTemp),
+        windSpeed: Math.round(windSpeed),
+        precip: Math.round(precip * 100) / 100,
+        iconCode,
     }
+}
+
+function parseDailyWeather({ daily }) {
+    
 }
